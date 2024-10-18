eba69cd813f11fed612e4db3bfc7c87684071ef4
enrico200165 <enrico200165@gmail.com>
2024-10-18 07:15:41 +0200

    saving

--- w3cschool/0100_PHP Tutorial/280_php_operators.php
+++ w3cschool/0100_PHP Tutorial/280_php_operators.php
@@ -6,6 +6,56 @@ define("ENDL", "<br/>\n");
 function nl() { echo ENDL;}
 
 
+nl();
+// **	Exponentiation	$x ** $y	Result of raising $x to the $y'th power	
+$x = 10;  
+$y = 3;
+echo $x ** $y; nl();
+
+
+nl();
+/* PHP Assignment Operators
+The PHP assignment operators are used with numeric values to write a value to a variable.
+
+The basic assignment operator in PHP is "=". It means that the left operand gets set to the value of the assignment expression on the right.
+
+Assignment	Same as...	Description	Try it
+x = y	x = y	The left operand gets set to the value of the expression on the right	
+x += y	x = x + y	Addition	
+x -= y	x = x - y	Subtraction	
+x *= y	x = x * y	Multiplication	
+x /= y	x = x / y	Division	
+x %= y	x = x % y	Modulus 
+*/
+
+$x = 10;
+$x /= 5;
+
+echo $x;
+
+
+$x = 15;
+$x %= 4;
+
+echo $x;
+
+nl();
+/* PHP Comparison Operators
+The PHP comparison operators are used to compare two values (number or string):
+
+Operator	Name	Example	Result	Try it
+==	Equal	$x == $y	Returns true if $x is equal to $y	
+===	Identical	$x === $y	Returns true if $x is equal to $y, and they are of the same type	
+!=	Not equal	$x != $y	Returns true if $x is not equal to $y	
+<>	Not equal	$x <> $y	Returns true if $x is not equal to $y	
+!==	Not identical	$x !== $y	Returns true if $x is not equal to $y, or they are not of the same type	
+>	Greater than	$x > $y	Returns true if $x is greater than $y	
+<	Less than	$x < $y	Returns true if $x is less than $y	
+>=	Greater than or equal to	$x >= $y	Returns true if $x is greater than or equal to $y	
+<=	Less than or equal to	$x <= $y	Returns true if $x is less than or equal to $y	
+<=>	Spaceship	$x <=> $y	Returns an integer less than, equal to, or greater than zero, depending on if $x is less than, equal to, or greater than $y. Introduced in PHP 7.
+*/
+
 // ==	Equal	$x == $y	Returns true if $x is equal to $y
 $x = 100;  
 $y = "100";
@@ -59,6 +109,7 @@ nl();
 
 
 
+
 nl();
 /* PHP Increment / Decrement Operators
 The PHP increment operators are used to increment a variable's value.
@@ -76,6 +127,25 @@ echo ++$x; nl();
 
 $x = 10;  
 echo $x++; nl();
+
+
+
+
+nl();
+/* PHP Logical Operators
+The PHP logical operators are used to combine conditional statements.
+
+Operator	Name	Example	Result	Try it
+and	And	$x and $y	True if both $x and $y are true	
+or	Or	$x or $y	True if either $x or $y is true	
+xor	Xor	$x xor $y	True if either $x or $y is true, but not both	
+&&	And	$x && $y	True if both $x and $y are true	
+||	Or	$x || $y	True if either $x or $y is true	
+!	Not	!$x	True if $x is not true
+*/
+
+
+
 
 
 nl();
@@ -119,6 +189,8 @@ $y = array("a" => "red", "b" => "green");
 $z = array("b" => "green", "a" => "red"); 
 var_dump($x !== $y);
 var_dump($x !== $z);
+
+
 
 
 
